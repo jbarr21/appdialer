e0b21f11a07cbfe018bd76e248843f0ed5f03661
James Barr <barr.james.r@gmail.com>
2022-11-06 05:39:23 -0800

    Version upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
-val accompanistVersion = "0.24.10-beta"
-val composeVersion = "1.2.0-beta03"
-val coroutinesVersion = "1.6.2"
+val accompanistVersion = "0.27.0"
+val composeVersion = "1.3.0"
+val coroutinesVersion = "1.6.4"
 val flipperVersion = "0.55.0"
-val hiltVersion = "2.36"
-val kotlinVersion = "1.6.21"
+val hiltVersion = "2.44"
+val kotlinVersion = "1.7.10"
 val roomVersion = "2.4.2"
 
 plugins {
@@ -14,12 +14,12 @@ plugins {
 }
 
 android {
-  compileSdkVersion(32)
+  compileSdkVersion(33)
   buildToolsVersion = "30.0.3"
   defaultConfig {
     applicationId = "io.github.jbarr21.appdialer"
     minSdkVersion(23)
-    targetSdkVersion(32)
+    targetSdkVersion(33)
     versionCode = 2
     versionName = "0.0.3"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -37,6 +37,11 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  packagingOptions {
+    resources {
+      excludes += "META-INF/kotlinx_coroutines_core.version"
+    }
   }
   composeOptions {
     kotlinCompilerExtensionVersion = composeVersion
@@ -56,27 +61,27 @@ android {
 }
 
 dependencies {
-  kapt("androidx.room:room-compiler:2.4.2")
+  kapt("androidx.room:room-compiler:2.4.3")
   kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
-  kapt("androidx.lifecycle:lifecycle-compiler:2.4.1")
+  kapt("androidx.lifecycle:lifecycle-compiler:2.5.1")
 
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-  implementation("androidx.activity:activity-compose:1.4.0")
-  implementation("androidx.appcompat:appcompat:1.4.2")
+  implementation("androidx.activity:activity-compose:1.6.1")
+  implementation("androidx.appcompat:appcompat:1.5.1")
   implementation("androidx.compose.ui:ui:$composeVersion")
   implementation("androidx.compose.ui:ui-tooling:$composeVersion")
   implementation("androidx.compose.foundation:foundation:$composeVersion")
   implementation("androidx.compose.material:material:$composeVersion")
   implementation("androidx.compose.material:material-icons-core:$composeVersion")
   implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-  implementation("androidx.core:core-ktx:1.8.0")
+  implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.datastore:datastore-preferences:1.0.0")
   implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
   implementation("com.google.dagger:hilt-android:$hiltVersion")
   implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-  implementation("androidx.navigation:navigation-compose:2.5.0-rc01")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+  implementation("androidx.navigation:navigation-compose:2.5.3")
   implementation("androidx.palette:palette:1.0.0")
   implementation("androidx.room:room-ktx:$roomVersion")
   implementation("androidx.room:room-runtime:$roomVersion")
@@ -84,18 +89,18 @@ dependencies {
   implementation("com.google.accompanist:accompanist-navigation-animation:$accompanistVersion")
   implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
   implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
-  implementation("com.google.android.material:material:1.6.1")
-  implementation("com.jakewharton:process-phoenix:2.0.0")
-  implementation("com.jakewharton.timber:timber:4.7.1")
+  implementation("com.google.android.material:material:1.7.0")
+  implementation("com.jakewharton:process-phoenix:2.1.2")
+  implementation("com.jakewharton.timber:timber:5.0.1")
   implementation("com.squareup.okhttp3:okhttp:4.9.2")
   implementation("io.coil-kt:coil-compose:1.4.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
   debugImplementation("com.facebook.flipper:flipper:$flipperVersion")
   debugImplementation("com.facebook.flipper:flipper-network-plugin:$flipperVersion")
-  debugImplementation("com.facebook.soloader:soloader:0.9.0")
+  debugImplementation("com.facebook.soloader:soloader:0.10.3")
 
-  testImplementation("com.google.truth:truth:1.1")
+  testImplementation("com.google.truth:truth:1.1.3")
   testImplementation("junit:junit:4.13.2")
 
   androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-  val kotlinVersion = "1.6.21"
-  val hiltVersion = "2.36"
+  val kotlinVersion = "1.7.10"
+  val hiltVersion = "2.44"
   repositories {
     google()
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.0.4")
+    classpath("com.android.tools.build:gradle:7.3.1")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
   }
